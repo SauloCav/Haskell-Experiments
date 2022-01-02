f06bf8cc92ce87065ce9bb188870d72f5fe1f65d
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-01-02 18:52:28 -0300

    Update Lab9.hs

--- Lab 09/Lab9.hs
+++ Lab 09/Lab9.hs
@@ -16,6 +16,50 @@ import qualified SeqTree as ST
 import qualified BSTree as BT
 import Either
 
+ask :: [Char] -> IO [Char]
+ask _ = putStrLn "Enter your password: " *> getLine
+
+getPasswd :: IO ([Char], [Char])
+getPasswd = do
+	p1 <- ask ""
+	p2 <- ask ""
+	-- pure ((,) <$> p1 <*> p2) 'failed idea'
+	pure (p1, p2)
+
+validPasswd :: IO [Char]
+validPasswd = getPasswd >>= f where
+	f (p1 , p2) = cond (p1 == p2) (pure p1) (incorrect *> validPasswd)
+	incorrect = putStrLn "They don't match! Try again"
+
+isStrong :: [Char] -> Bool
+isStrong [] = False
+isStrong (xs) = cond (opAnd (opAnd (hasUppercase xs) (hasLowercase xs)) (opAnd (isLongEnough xs) (hasDigit xs))) (True) (False)
+
+hasUppercase :: [Char] -> Bool
+hasUppercase [] = False
+hasUppercase (xs) = cond (or (map isLower xs)) (True) (False)
+
+hasLowercase :: [Char] -> Bool
+hasLowercase [] = False
+hasLowercase (xs) = cond (or (map isUpper xs)) (True) (False)
+
+isLongEnough :: [Char] -> Bool
+isLongEnough [] = False
+isLongEnough (xs) = cond (length xs >= 8) (True) (False)
+
+hasDigit :: [Char] -> Bool
+hasDigit [] = False
+hasDigit (xs) = cond (or (map isDigit xs)) (True) (False)
+
+strongPasswd :: IO [Char]
+strongPasswd = validPasswd >>= f where
+	f (xs) = cond (isStrong xs) (pure xs) (incorrect *> strongPasswd)
+	incorrect = putStrLn "Weak password! Try again"
+
+main = strongPasswd
+
+-- Extra questions
+
 -- Quest 23
 -- instance Functor (ST.SeqTree k) where
     -- fmap = map
@@ -65,45 +109,3 @@ import Either
 -- Quest 31
 -- asum :: Alternative f => [f a] -> f a
 -- asum = foldr (<*>) empty
-
-ask :: [Char] -> IO [Char]
-ask _ = putStrLn "Enter your password: " *> getLine
-
-getPasswd :: IO ([Char], [Char])
-getPasswd = do
-	p1 <- ask ""
-	p2 <- ask ""
-	-- pure ((,) <$> p1 <*> p2) 'failed idea'
-	pure (p1, p2)
-
-validPasswd :: IO [Char]
-validPasswd = getPasswd >>= f where
-	f (p1 , p2) = cond (p1 == p2) (pure p1) (incorrect *> validPasswd)
-	incorrect = putStrLn "They don't match! Try again"
-
-isStrong :: [Char] -> Bool
-isStrong [] = False
-isStrong (xs) = cond (opAnd (opAnd (hasUppercase xs) (hasLowercase xs)) (opAnd (isLongEnough xs) (hasDigit xs))) (True) (False)
-
-hasUppercase :: [Char] -> Bool
-hasUppercase [] = False
-hasUppercase (xs) = cond (or (map isLower xs)) (True) (False)
-
-hasLowercase :: [Char] -> Bool
-hasLowercase [] = False
-hasLowercase (xs) = cond (or (map isUpper xs)) (True) (False)
-
-isLongEnough :: [Char] -> Bool
-isLongEnough [] = False
-isLongEnough (xs) = cond (length xs >= 8) (True) (False)
-
-hasDigit :: [Char] -> Bool
-hasDigit [] = False
-hasDigit (xs) = cond (or (map isDigit xs)) (True) (False)
-
-strongPasswd :: IO [Char]
-strongPasswd = validPasswd >>= f where
-	f (xs) = cond (isStrong xs) (pure xs) (incorrect *> strongPasswd)
-	incorrect = putStrLn "Weak password! Try again"
-
-main = strongPasswd
